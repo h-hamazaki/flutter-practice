3958fe4434279938b3e690da8b9f9da54146153b
h-hamazaki <homare.hamazaki@myu-on.co.jp>
2021-03-16 17:26:29 +0900

    TestScreen を追加した

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         '/stateful': (BuildContext context) => MyStatefulScreen(title: 'This Is StatefulWidget'),
         '/stateless': (BuildContext context) => MyStatelessScreen(title: 'This Is StatelessWidget'),
         '/provider': (BuildContext context) => ProviderTestScreen(title: 'This Is Provider'),
+        '/test': (BuildContext context) => TestScreen(),
       },
       home: HomeScreen(),
     );
@@ -38,9 +39,11 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            transitionButton(context, "Stateful", Colors.red, () => Navigator.pushNamed(context, "/stateful")),
+            transitionButton(context, "Stateful", Colors.red,
+                () => Navigator.pushNamed(context, "/stateful", arguments: "hogehoge")),
             transitionButton(context, "Stateless", Colors.blue, () => Navigator.pushNamed(context, "/stateless")),
             transitionButton(context, "Provider", Colors.green, () => Navigator.pushNamed(context, "/provider")),
+            transitionButton(context, "Test", Colors.purple, () => Navigator.pushNamed(context, "/test")),
           ],
         ),
       ),
@@ -52,6 +55,55 @@ class HomeScreen extends StatelessWidget {
       width: MediaQuery.of(context).size.width,
       child: RaisedButton(
           onPressed: callback, color: themeColor, child: Text(headerTitle, style: TextStyle(color: Colors.white))),
+    );
+  }
+}
+
+class TestScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Test"),
+        backgroundColor: Colors.purple,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(height: 80.0, color: Colors.red),
+            Container(height: 80.0, color: Colors.red[400]),
+            Container(height: 80.0, color: Colors.red[300]),
+            Container(
+              height: 200.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(width: 80.0, color: Colors.blue),
+                  Container(width: 80.0, color: Colors.blue[400]),
+                  Container(width: 80.0, color: Colors.blue[300]),
+                  Container(width: 80.0, color: Colors.blue[200]),
+                ],
+              ),
+            ),
+            SizedBox(height: 20.0),
+            RaisedButton(
+              child: Text("This is Button"),
+              color: Colors.yellow,
+              onPressed: () {
+                debugPrint("Pushed!!!");
+              },
+            ),
+            SizedBox(height: 20.0),
+            Text("BIG TEXT", style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold))
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 80.0,
+        color: Colors.green,
+        child: Text("This is Bottom Area"),
+      ),
     );
   }
 }
